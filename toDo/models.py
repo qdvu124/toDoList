cb5b336ed91a1523ebbe6844c28fd6b720643dd9
Quan Vu <qvu@colgate.edu>
2016-03-17 17:30:56 -0400

    Default: Not completed

--- toDo/models.py
+++ toDo/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 
 class toDoItem(models.Model):
     task = models.CharField(max_length=200, default='')
     deadline = models.DateTimeField(default=timezone.now)
-    isCompleted = models.BooleanField(default="false")
+    isCompleted = models.BooleanField(default=False)
 
     def post(self):
         self.deadline = timezone.now()
