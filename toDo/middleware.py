8e22a11f2a0dfbbe24c4c4f60075c6b13828bed6
Quan Vu <qvu@colgate.edu>
2016-03-18 23:34:27 -0400

    Separated Login and Register forms

--- toDo/middleware.py
+++ toDo/middleware.py
@@ -3,9 +3,11 @@ from django.http import HttpRequest
 from django.shortcuts import redirect
 from re import compile
 
+
 class LoginRequiredMiddleware:
     def process_request(self, request):
         if request.session.get('user_id', None) is None:
             if 'login' not in request.path:
-                return redirect('login')
+                if 'register' not in request.path:
+                    return redirect('login')
         return
--- toDo/views.py
+++ toDo/views.py
@@ -65,31 +65,35 @@ def log_in(request):
         return redirect('post_list')
     if request.method == "POST":
         form = LoginForm(request.POST)
-        if request.POST['Button'] == 'Register':
-            if form.is_valid():
-                username = form.cleaned_data['username']
-
-                userlist = User.objects.filter(username=username)
-
-                if userlist.count() == 0:
-                    form.save()
-                    return redirect('post_list')
-        else:
-            if form.is_valid():
-                username = form.cleaned_data['username']
-                password = form.cleaned_data['password']
-                request.session['username'] = username
-                logger.error(request.session.session_key)
-                logger.error(request.session['username'])
-                user = get_object_or_404(User, username=username)
-                if password != user.password:
-                    form = LoginForm()
-                    return render(request, 'toDo/login.html', {'form': form})
-                request.session['user_id'] = user.pk
-                request.session.set_expiry(300)
-                return redirect('post_list')
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            request.session['username'] = username
+            user = get_object_or_404(User, username=username)
+            if password != user.password:
+                form = LoginForm()
+                return render(request, 'toDo/login.html', {'form': form})
+            request.session['user_id'] = user.pk
+            request.session.set_expiry(300)
+            return redirect('post_list')
     form = LoginForm()
     return render(request, 'toDo/login.html', {'form': form})
+
+
+@never_cache
+def register(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            userlist = User.objects.filter(username=username)
+            if userlist.count() == 0:
+                form.save()
+                logger.error(get_object_or_404(User, username=username).pk)
+                request.session['user_id'] = get_object_or_404(User, username=username).pk
+                return redirect('post_list')
+    form = LoginForm()
+    return render(request, 'toDo/register.html', {'form': form})
 
 
 @never_cache
